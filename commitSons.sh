d05b7ecd2040aea33f0231e857e36a360323c012
Joris <jorisgirardjoris@gmail.com>
2021-04-17 12:30:37 +0200

    init sons du jour

--- commitSons.sh
+++ commitSons.sh
@@ -4,14 +4,27 @@ git pull
 
 rm sons.json
 
+rm sonsb.json
+
 curl http://192.168.1.22/son -o sons.json
+curl http://192.168.1.22/sonb -o sonsb.json
 
 if [[ `git status ./sons.json --porcelain` ]]; then
     echo "Fichié modifié, commit des changements..."
     git add ./sons.json
     git commit -m "Modification du fichier sons.json - `date +'%d/%m/%Y'`"
-    git push
     echo "Fichié envoyé."
 else
-    echo "Le fichier Music n'a pas été modifié."
+    echo "Le fichier sons n'a pas été modifié."
 fi
+
+if [[ `git status ./sonsb.json --porcelain` ]]; then
+    echo "Fichié modifié, commit des changements..."
+    git add ./sonsb.json
+    git commit -m "Modification du fichier sonsb.json - `date +'%d/%m/%Y'`"
+    echo "Fichié envoyé."
+else
+    echo "Le fichier sonb n'a pas été modifié."
+fi
+
+git push
