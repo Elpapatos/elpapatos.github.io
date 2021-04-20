63a7e208bef25e8cb9271f1f7f456e83b7f7f0e7
Joris <jorisgirardjoris@gmail.com>
2021-04-20 18:09:15 +0200

    script sons du jour + inti users json

---
+++ setSonsDuJour.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+git pull
+
+while :; do
+    read -ep 'Nombre de sons du jour (1-10) : ' number
+    [[ $number =~ ^[[:digit:]]+$ ]] || continue
+    (( ( (number=(10#$number)) <= 10 ) && number >= 1 )) || continue
+    break
+done
+
+> sonsdujour.json
+
+echo "[" >> sonsdujour.json
+
+for i in $(seq 1 "$number") 
+do
+    read son
+    echo "\"$son\"" >> sonsdujour.json
+    if [ "$i" = "$number" ]; then
+        continue
+    else
+        echo "," >> sonsdujour.json
+    fi
+done
+
+echo "]" >> sonsdujour.json
+
+if [[ `git status ./sonsdujour.json --porcelain` ]]; then
+    echo "Fichié modifié, commit des changements..."
+    git add ./sonsdujour.json
+    git commit -m "Modification du fichier sonsdujour.json - `date +'%d/%m/%Y'`"
+    git push
+    echo "Fichié envoyé."
+else
+    echo "Le fichier sonsdujour n'a pas été modifié."
+fi
+
+echo "Sons du jour mis à jour, appyer sur une touche pour continuer..."
+read nbrSonJour
